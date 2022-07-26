f1232007c387fa7d1c9eff6f9fc698b931b2c28b
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-07-26 19:41:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("code-ex1").innerText = `
   </head>
   <body>
     <h1>Header</h1>
-    <p>The html in this example is the same as in the other two.</p>
+    <p>The HTML in this example is the same as in the other two, except for CSS and JS references.</p>
     <span>Counter: <b id="count">0</b>.</span>
   </body>
 </html>
