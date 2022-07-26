44c5f84210914b88802dedd72bb1580f991045f5
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-07-26 19:06:24 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("code-ex1").innerText = `
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <h1>Header</h1>
+    <p>The html in this example is the same as in the other two.</p>
+    <span>Counter: <b id="count">0</b>.</span>
+  </body>
+</html>
+`;
